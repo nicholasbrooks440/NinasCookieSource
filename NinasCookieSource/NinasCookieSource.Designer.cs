45ad10a21be08646f841a052a398c416a4f3bedb
Nick Brooks <nic9212or@gmail.com>
2017-08-20 22:10:47 -0400

    Add project files.

---
+++ NinasCookieSource/NinasCookieSource.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace NinasCookieSource
+{
+    partial class NinasCookieSource
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chocChipRadioBtn = new System.Windows.Forms.RadioButton();
+            this.oatmealRadioBtn = new System.Windows.Forms.RadioButton();
+            this.vanWafRadioBtn = new System.Windows.Forms.RadioButton();
+            this.twoDozRadioBtn = new System.Windows.Forms.RadioButton();
+            this.oneDozRadioBtn = new System.Windows.Forms.RadioButton();
+            this.halfDozRadioBtn = new System.Windows.Forms.RadioButton();
+            this.threeDozRadioBtn = new System.Windows.Forms.RadioButton();
+            this.cookieDelvDate = new System.Windows.Forms.Label();
+            this.ttlPriceLabel = new System.Windows.Forms.Label();
+            this.quantityGroupBox = new System.Windows.Forms.GroupBox();
+            this.cookieGroupBox = new System.Windows.Forms.GroupBox();
+            this.cookieDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.quantityGroupBox.SuspendLayout();
+            this.cookieGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chocChipRadioBtn
+            // 
+            this.chocChipRadioBtn.AutoSize = true;
+            this.chocChipRadioBtn.Location = new System.Drawing.Point(9, 29);
+            this.chocChipRadioBtn.Name = "chocChipRadioBtn";
+            this.chocChipRadioBtn.Size = new System.Drawing.Size(97, 17);
+            this.chocChipRadioBtn.TabIndex = 0;
+            this.chocChipRadioBtn.TabStop = true;
+            this.chocChipRadioBtn.Text = "Chocolate Chip";
+            this.chocChipRadioBtn.UseVisualStyleBackColor = true;
+            this.chocChipRadioBtn.CheckedChanged += new System.EventHandler(this.chocChipRadioBtn_CheckedChanged);
+            // 
+            // oatmealRadioBtn
+            // 
+            this.oatmealRadioBtn.AutoSize = true;
+            this.oatmealRadioBtn.Location = new System.Drawing.Point(9, 58);
+            this.oatmealRadioBtn.Name = "oatmealRadioBtn";
+            this.oatmealRadioBtn.Size = new System.Drawing.Size(64, 17);
+            this.oatmealRadioBtn.TabIndex = 1;
+            this.oatmealRadioBtn.TabStop = true;
+            this.oatmealRadioBtn.Text = "Oatmeal";
+            this.oatmealRadioBtn.UseVisualStyleBackColor = true;
+            this.oatmealRadioBtn.CheckedChanged += new System.EventHandler(this.oatmealRadioBtn_CheckedChanged);
+            // 
+            // vanWafRadioBtn
+            // 
+            this.vanWafRadioBtn.AutoSize = true;
+            this.vanWafRadioBtn.Location = new System.Drawing.Point(9, 90);
+            this.vanWafRadioBtn.Name = "vanWafRadioBtn";
+            this.vanWafRadioBtn.Size = new System.Drawing.Size(91, 17);
+            this.vanWafRadioBtn.TabIndex = 2;
+            this.vanWafRadioBtn.TabStop = true;
+            this.vanWafRadioBtn.Text = "Vanilla Waffer";
+            this.vanWafRadioBtn.UseVisualStyleBackColor = true;
+            this.vanWafRadioBtn.CheckedChanged += new System.EventHandler(this.vanWafRadioBtn_CheckedChanged);
+            // 
+            // twoDozRadioBtn
+            // 
+            this.twoDozRadioBtn.AutoSize = true;
+            this.twoDozRadioBtn.Location = new System.Drawing.Point(30, 85);
+            this.twoDozRadioBtn.Name = "twoDozRadioBtn";
+            this.twoDozRadioBtn.Size = new System.Drawing.Size(80, 17);
+            this.twoDozRadioBtn.TabIndex = 5;
+            this.twoDozRadioBtn.TabStop = true;
+            this.twoDozRadioBtn.Text = "Two Dozen";
+            this.twoDozRadioBtn.UseVisualStyleBackColor = true;
+            this.twoDozRadioBtn.CheckedChanged += new System.EventHandler(this.twoDozRadioBtn_CheckedChanged);
+            // 
+            // oneDozRadioBtn
+            // 
+            this.oneDozRadioBtn.AutoSize = true;
+            this.oneDozRadioBtn.Location = new System.Drawing.Point(31, 53);
+            this.oneDozRadioBtn.Name = "oneDozRadioBtn";
+            this.oneDozRadioBtn.Size = new System.Drawing.Size(79, 17);
+            this.oneDozRadioBtn.TabIndex = 4;
+            this.oneDozRadioBtn.TabStop = true;
+            this.oneDozRadioBtn.Text = "One Dozen";
+            this.oneDozRadioBtn.UseVisualStyleBackColor = true;
+            this.oneDozRadioBtn.CheckedChanged += new System.EventHandler(this.oneDozRadioBtn_CheckedChanged);
+            // 
+            // halfDozRadioBtn
+            // 
+            this.halfDozRadioBtn.AutoSize = true;
+            this.halfDozRadioBtn.Location = new System.Drawing.Point(31, 19);
+            this.halfDozRadioBtn.Name = "halfDozRadioBtn";
+            this.halfDozRadioBtn.Size = new System.Drawing.Size(76, 17);
+            this.halfDozRadioBtn.TabIndex = 3;
+            this.halfDozRadioBtn.TabStop = true;
+            this.halfDozRadioBtn.Text = "1/2 Dozen";
+            this.halfDozRadioBtn.UseVisualStyleBackColor = true;
+            this.halfDozRadioBtn.CheckedChanged += new System.EventHandler(this.halfDozRadioBtn_CheckedChanged);
+            // 
+            // threeDozRadioBtn
+            // 
+            this.threeDozRadioBtn.AutoSize = true;
+            this.threeDozRadioBtn.Location = new System.Drawing.Point(31, 117);
+            this.threeDozRadioBtn.Name = "threeDozRadioBtn";
+            this.threeDozRadioBtn.Size = new System.Drawing.Size(87, 17);
+            this.threeDozRadioBtn.TabIndex = 6;
+            this.threeDozRadioBtn.TabStop = true;
+            this.threeDozRadioBtn.Text = "Three Dozen";
+            this.threeDozRadioBtn.UseVisualStyleBackColor = true;
+            this.threeDozRadioBtn.CheckedChanged += new System.EventHandler(this.threeDozRadioBtn_CheckedChanged);
+            // 
+            // cookieDelvDate
+            // 
+            this.cookieDelvDate.AutoSize = true;
+            this.cookieDelvDate.Location = new System.Drawing.Point(98, 241);
+            this.cookieDelvDate.Name = "cookieDelvDate";
+            this.cookieDelvDate.Size = new System.Drawing.Size(0, 13);
+            this.cookieDelvDate.TabIndex = 8;
+            // 
+            // ttlPriceLabel
+            // 
+            this.ttlPriceLabel.AutoSize = true;
+            this.ttlPriceLabel.Location = new System.Drawing.Point(98, 296);
+            this.ttlPriceLabel.Name = "ttlPriceLabel";
+            this.ttlPriceLabel.Size = new System.Drawing.Size(0, 13);
+            this.ttlPriceLabel.TabIndex = 9;
+            // 
+            // quantityGroupBox
+            // 
+            this.quantityGroupBox.Controls.Add(this.halfDozRadioBtn);
+            this.quantityGroupBox.Controls.Add(this.oneDozRadioBtn);
+            this.quantityGroupBox.Controls.Add(this.twoDozRadioBtn);
+            this.quantityGroupBox.Controls.Add(this.threeDozRadioBtn);
+            this.quantityGroupBox.Location = new System.Drawing.Point(261, 43);
+            this.quantityGroupBox.Name = "quantityGroupBox";
+            this.quantityGroupBox.Size = new System.Drawing.Size(148, 152);
+            this.quantityGroupBox.TabIndex = 10;
+            this.quantityGroupBox.TabStop = false;
+            this.quantityGroupBox.Text = "Quantity";
+         
+            // 
+            // cookieGroupBox
+            // 
+            this.cookieGroupBox.Controls.Add(this.chocChipRadioBtn);
+            this.cookieGroupBox.Controls.Add(this.oatmealRadioBtn);
+            this.cookieGroupBox.Controls.Add(this.vanWafRadioBtn);
+            this.cookieGroupBox.Location = new System.Drawing.Point(55, 43);
+            this.cookieGroupBox.Name = "cookieGroupBox";
+            this.cookieGroupBox.Size = new System.Drawing.Size(128, 144);
+            this.cookieGroupBox.TabIndex = 11;
+            this.cookieGroupBox.TabStop = false;
+            this.cookieGroupBox.Text = "Cookie Type:";
+            // 
+            // cookieDateTimePicker
+            // 
+            this.cookieDateTimePicker.Location = new System.Drawing.Point(510, 43);
+            this.cookieDateTimePicker.Name = "cookieDateTimePicker";
+            this.cookieDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.cookieDateTimePicker.TabIndex = 12;
+            this.cookieDateTimePicker.ValueChanged += new System.EventHandler(this.cookieDateTimePicker_ValueChanged);
+            // 
+            // NinasCookieSource
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 377);
+            this.Controls.Add(this.cookieDateTimePicker);
+            this.Controls.Add(this.cookieGroupBox);
+            this.Controls.Add(this.quantityGroupBox);
+            this.Controls.Add(this.ttlPriceLabel);
+            this.Controls.Add(this.cookieDelvDate);
+            this.Name = "NinasCookieSource";
+            this.Text = "Nina\'s Cookie Source";
+            this.quantityGroupBox.ResumeLayout(false);
+            this.quantityGroupBox.PerformLayout();
+            this.cookieGroupBox.ResumeLayout(false);
+            this.cookieGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton chocChipRadioBtn;
+        private System.Windows.Forms.RadioButton oatmealRadioBtn;
+        private System.Windows.Forms.RadioButton vanWafRadioBtn;
+        private System.Windows.Forms.RadioButton twoDozRadioBtn;
+        private System.Windows.Forms.RadioButton oneDozRadioBtn;
+        private System.Windows.Forms.RadioButton halfDozRadioBtn;
+        private System.Windows.Forms.RadioButton threeDozRadioBtn;
+        private System.Windows.Forms.Label cookieDelvDate;
+        private System.Windows.Forms.Label ttlPriceLabel;
+        private System.Windows.Forms.GroupBox quantityGroupBox;
+        private System.Windows.Forms.GroupBox cookieGroupBox;
+        private System.Windows.Forms.DateTimePicker cookieDateTimePicker;
+    }
+}
+
---
+++ NinasCookieSource/NinasCookieSource.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NinasCookieSource
+{
+    public partial class NinasCookieSource : Form
+    {
+        private double cookiePrice;
+        private double quantity;
+        private double totalPrice;
+        
+        public NinasCookieSource()
+        {
+            InitializeComponent();
+          
+            //set to true
+            chocChipRadioBtn.Checked = true;
+            oneDozRadioBtn.Checked = true;
+            ttlPriceLabel.Text = "Total price is: " + 8.99; //default selection price = 8.99
+            
+            //Set min Date to 3 days ahead and max date 10 days from now
+            cookieDateTimePicker.MinDate = DateTime.Now.AddDays(3);
+            cookieDateTimePicker.MaxDate = DateTime.Now.AddDays(10);
+
+            var qantityRadio = quantityGroupBox.Controls.OfType<RadioButton>()
+                                      .FirstOrDefault(r => r.Checked);
+
+        }
+
+        public double CookiePrice
+        {
+            get { return cookiePrice; }
+            set { cookiePrice = value; }
+        }
+        public double Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        /////////////////////
+        private void cookieDateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            //need to drop the seconds
+            String userDate = cookieDateTimePicker.Value.ToString("MM/dd/yyyy");
+
+            cookieDelvDate.Text = "Cookies will be delivered on : " + userDate;
+        }
+
+        //Cookie Radio buttons
+        private void oatmealRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            const double OATMEAL_PRICE = 6.99;
+            if (oatmealRadioBtn.Checked == true)
+            {
+                CookiePrice = OATMEAL_PRICE;
+            }
+            totalPrice = CookiePrice * Quantity;
+            ttlPriceLabel.Text = "Total price is: " + totalPrice.ToString("C");          
+        }
+
+        private void vanWafRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            const double VANWAF_PRICE = 6.49;
+            if (vanWafRadioBtn.Checked == true)
+            {
+                CookiePrice = VANWAF_PRICE;   
+            }
+            totalPrice = CookiePrice * Quantity;
+            ttlPriceLabel.Text = "Total price is: " + totalPrice.ToString("C");       
+        }
+
+        private void chocChipRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            const double CHOCCHIP_PRICE = 8.99;
+            if (chocChipRadioBtn.Checked == true)
+            {
+                CookiePrice = CHOCCHIP_PRICE;                
+            }
+            totalPrice = CookiePrice * Quantity;
+            ttlPriceLabel.Text = "Total price is: " + totalPrice.ToString("C");       
+        }
+        /// QTY Radio Buttons
+      
+        private void halfDozRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            const double HALF_DOZ = .5;
+            if (halfDozRadioBtn.Checked == true)
+            {
+                Quantity = HALF_DOZ;
+                totalPrice = CookiePrice * Quantity;
+                ttlPriceLabel.Text = "Total price is: " + totalPrice.ToString("C");        
+            }
+        }
+
+        private void oneDozRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            const double ONE_DOZ = 1;
+            if (oneDozRadioBtn.Checked == true)
+            {
+                Quantity = ONE_DOZ;
+                totalPrice = CookiePrice * Quantity;
+                ttlPriceLabel.Text = "Total price is: " + totalPrice.ToString("C"); 
+            }
+        }
+
+        private void twoDozRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            const double TWO_DOZ = 2;
+            if (twoDozRadioBtn.Checked == true)
+            {
+                Quantity = TWO_DOZ;
+                totalPrice = CookiePrice * Quantity;
+                ttlPriceLabel.Text = "Total price is: " + totalPrice.ToString("C"); 
+            }
+        }
+
+        private void threeDozRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            const double THREE_DOZ = 3;
+            if (threeDozRadioBtn.Checked == true)
+            {
+                Quantity = THREE_DOZ;
+                totalPrice = CookiePrice * Quantity;
+                ttlPriceLabel.Text = "Total price is: " + totalPrice.ToString("C");    
+            }
+        }
+
+    }
+}
